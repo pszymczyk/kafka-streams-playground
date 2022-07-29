cf488bb5475941c315e8a9b3277cf0fb96bda004
Pawel Szymczyk <pawel.szymczyk@displate.com>
2022-07-29 22:14:33 +0200

    simplify join app

---
+++ src/main/java/com/pszymczyk/app4/UserFriendlyMessagesApp.java
@@ -0,0 +1,39 @@
+package com.pszymczyk.app4;
+
+import com.pszymczyk.common.StreamsRunner;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KTable;
+
+import java.util.Map;
+
+class UserFriendlyMessagesApp {
+
+    static final String MESSAGES = "messages";
+    static final String USERS = "users";
+    static final String USER_FRIENDLY_MESSAGES = "user-friendly-messages";
+
+    public static void main(String[] args) {
+        StreamsBuilder builder = buildKafkaStreamsTopology();
+        new StreamsRunner().run(
+                "localhost:9092",
+                "user-friendly-messages-main",
+                builder,
+                Map.of(),
+                new NewTopic(MESSAGES, 1, (short) 1),
+                new NewTopic(USER_FRIENDLY_MESSAGES, 1, (short) 1),
+                new NewTopic(USERS, 1, (short) 1));
+    }
+
+    static StreamsBuilder buildKafkaStreamsTopology() {
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KTable<String, String> itemDetailsTable = builder.table(USERS);
+
+        builder.<String,String>stream(MESSAGES)
+                .join(itemDetailsTable, (message, user) -> message.replaceAll("#user", user))
+                .to(USER_FRIENDLY_MESSAGES);
+
+        return builder;
+    }
+}
---
+++ src/test/groovy/com/pszymczyk/app4/UserFriendlyMessagesAppSpec.groovy
@@ -0,0 +1,80 @@
+package com.pszymczyk.app4
+
+import com.pszymczyk.IntegrationSpec
+import com.pszymczyk.common.StreamsRunner
+import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.streams.KafkaStreams
+import spock.lang.Shared
+
+import java.time.Duration
+
+import static com.pszymczyk.app4.UserFriendlyMessagesApp.*
+
+class UserFriendlyMessagesAppSpec extends IntegrationSpec {
+
+    @Shared
+    KafkaStreams kafkaStreams
+
+    def setupSpec() {
+        kafkaStreams = new StreamsRunner().run(
+                bootstrapServers,
+                "user-friendly-messages-spec",
+                buildKafkaStreamsTopology(),
+                [:],
+                new NewTopic(USERS, 1, (short) 1),
+                new NewTopic(MESSAGES, 1, (short) 1),
+                new NewTopic(USER_FRIENDLY_MESSAGES, 1, (short) 1))
+    }
+
+    def cleanupSpec() {
+        kafkaStreams.close()
+    }
+
+    def "Should build orders with details state"() {
+        given:
+        kafkaConsumer.subscribe([USER_FRIENDLY_MESSAGES])
+
+        when: "send some user details"
+        sendToKafka(USERS, "123", "Pawel Szymczyk")
+        sendToKafka(USERS, "456", "Jan Kowalski")
+        sendToKafka(USERS, "789", "Anna Hiacynta")
+
+        and: "send some messages"
+        sendToKafka(MESSAGES, "123", "Hello #user, Here is some extra deal for you!")
+        sendToKafka(MESSAGES, "456", "Hi #user, Your order is completed.")
+        sendToKafka(MESSAGES, "789", "Alo #user, All the best in valentine's day.")
+
+        and: "collect all events"
+        Map<String, String> messages = [:]
+        5.times {
+            def consumerRecords = kafkaConsumer.poll(Duration.ofMillis(500))
+            logger.info("Received {} events", consumerRecords.size())
+            consumerRecords.each {
+                messages.put(it.key(), it.value())
+            }
+        }
+
+        then: "messages have user friendly format"
+        messages == ["123": "Hello Pawel Szymczyk, Here is some extra deal for you!",
+                     "456": "Hi Jan Kowalski, Your order is completed.",
+                     "789": "Alo Anna Hiacynta, All the best in valentine's day."]
+
+        when: "user details changed"
+        sendToKafka(USERS, "789", "Anna Kowalska")
+
+        and: "we collect all order events again"
+        def yetAnotherConsumer = kafkaConsumer("${this.class.simpleName}-2")
+        yetAnotherConsumer.subscribe([USER_FRIENDLY_MESSAGES])
+        def messagesAfterUserDetailsChanged = [:]
+        5.times {
+            def consumerRecords = yetAnotherConsumer.poll(Duration.ofMillis(500))
+            logger.info("Received {} events", consumerRecords.size())
+            consumerRecords.each {
+                messagesAfterUserDetailsChanged.put(it.key(), it.value())
+            }
+        }
+
+        then: "nothing happened in already sent messages"
+        messages == messagesAfterUserDetailsChanged
+    }
+}
