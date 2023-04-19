9f400221869023ef650b3ac107eb23144bb613b2
Pawel Szymczyk <pawel.szymczyk@displate.com>
2023-04-19 18:46:59 +0200

    update libraries

--- src/main/java/com/pszymczyk/app7/LoanApplicationProcess.java
+++ src/main/java/com/pszymczyk/app7/LoanApplicationProcess.java
@@ -1,25 +1,27 @@
 package com.pszymczyk.app7;
 
-import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.kstream.Transformer;
-import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.ProcessorContext;
+import org.apache.kafka.streams.processor.api.Record;
 import org.apache.kafka.streams.state.KeyValueStore;
 
 import java.math.BigDecimal;
 
 
-public class LoanApplicationProcess implements Transformer<String, LoanApplicationRequest, KeyValue<String, LoanApplicationDecision>> {
+public class LoanApplicationProcess implements Processor<String, LoanApplicationRequest, String, LoanApplicationDecision> {
 
     private KeyValueStore<String, Integer> usersLoansCount;
+    private ProcessorContext<String, LoanApplicationDecision> context;
 
     @Override
-    public void init(ProcessorContext context) {
+    public void init(ProcessorContext<String, LoanApplicationDecision> context) {
+        this.context = context;
         usersLoansCount = context.getStateStore("users-loans-count");
     }
 
     @Override
-    public KeyValue<String, LoanApplicationDecision> transform(String key, LoanApplicationRequest value) {
-        return processApplication(value);
+    public void process(Record<String, LoanApplicationRequest> record) {
+        context.forward(processApplication(record.value()));
     }
 
     @Override
@@ -28,7 +30,7 @@ public class LoanApplicationProcess implements Transformer<String, LoanApplicati
     }
 
 
-    private KeyValue<String, LoanApplicationDecision> processApplication(LoanApplicationRequest loanApplicationRequest) {
+    private Record<String, LoanApplicationDecision> processApplication(LoanApplicationRequest loanApplicationRequest) {
         Integer userLoansCount = usersLoansCount.get(loanApplicationRequest.getRequester());
 
         if (userLoansCount == null) {
@@ -36,19 +38,19 @@ public class LoanApplicationProcess implements Transformer<String, LoanApplicati
             LoanApplicationDecision loanApplicationDecision = new LoanApplicationDecision();
             loanApplicationDecision.setAmount(loanApplicationRequest.getAmount());
             loanApplicationDecision.setRequester(loanApplicationRequest.getRequester());
-            return new KeyValue<>(loanApplicationDecision.getRequester(), loanApplicationDecision);
+            return new Record<>(loanApplicationDecision.getRequester(), loanApplicationDecision, context.currentStreamTimeMs());
         } else if (userLoansCount == 1) {
             usersLoansCount.put(loanApplicationRequest.getRequester(), 2);
             LoanApplicationDecision loanApplicationDecision = new LoanApplicationDecision();
             loanApplicationDecision.setAmount(loanApplicationRequest.getAmount().multiply(new BigDecimal("0.8")));
             loanApplicationDecision.setRequester(loanApplicationRequest.getRequester());
-            return new KeyValue<>(loanApplicationDecision.getRequester(), loanApplicationDecision);
+            return new Record<>(loanApplicationDecision.getRequester(), loanApplicationDecision, context.currentStreamTimeMs());
         } else {
             usersLoansCount.put(loanApplicationRequest.getRequester(), ++userLoansCount);
             LoanApplicationDecision loanApplicationDecision = new LoanApplicationDecision();
             loanApplicationDecision.setAmount(loanApplicationRequest.getAmount().multiply(new BigDecimal("0.5")));
             loanApplicationDecision.setRequester(loanApplicationRequest.getRequester());
-            return new KeyValue<>(loanApplicationDecision.getRequester(), loanApplicationDecision);
+            return new Record<>(loanApplicationDecision.getRequester(), loanApplicationDecision, context.currentStreamTimeMs());
         }
     }
 
--- src/main/java/com/pszymczyk/app7/LoanApplicationProcessorApp.java
+++ src/main/java/com/pszymczyk/app7/LoanApplicationProcessorApp.java
@@ -7,6 +7,8 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.ProcessorSupplier;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.state.StoreBuilder;
 import org.apache.kafka.streams.state.Stores;
@@ -22,7 +24,7 @@ class LoanApplicationProcessorApp {
         StreamsBuilder builder = buildKafkaStreamsTopology();
         new StreamsRunner().run(
             "localhost:9092",
-            "LoanApplicationProcess-app-main",
+            "LoanApplicationProcess-app-main12",
             builder,
             Map.of(),
             new NewTopic(LOAN_APPLICATION_REQUESTS, 1, (short) 1),
@@ -37,7 +39,12 @@ class LoanApplicationProcessorApp {
         builder.addStateStore(transferProcessKeyValueStore);
 
         builder.stream(LOAN_APPLICATION_REQUESTS, Consumed.with(Serdes.String(), JsonSerdes.forA(LoanApplicationRequest.class)))
-            .transform(LoanApplicationProcess::new, "users-loans-count")
+            .process(new ProcessorSupplier<String, LoanApplicationRequest, String, LoanApplicationDecision>() {
+                @Override
+                public Processor<String, LoanApplicationRequest, String, LoanApplicationDecision> get() {
+                    return new LoanApplicationProcess();
+                }
+            }, "users-loans-count")
             .to(LOAN_APPLICATION_DECISIONS, Produced.with(Serdes.String(), JsonSerdes.forA(LoanApplicationDecision.class)));
 
         return builder;
