8843dab0793e669f98afd774ba3801883d66ea24
Pawel Szymczyk <pawel@pszymczyk.com>
2023-11-08 14:53:07 +0100

    remove impl

--- src/main/java/com/pszymczyk/app7/LoanApplicationProcess.java
+++ src/main/java/com/pszymczyk/app7/LoanApplicationProcess.java
@@ -21,37 +21,11 @@ public class LoanApplicationProcess implements Processor<String, LoanApplication
 
     @Override
     public void process(Record<String, LoanApplicationRequest> record) {
-        context.forward(processApplication(record.value()));
+
     }
 
     @Override
     public void close() {
 
     }
-
-
-    private Record<String, LoanApplicationDecision> processApplication(LoanApplicationRequest loanApplicationRequest) {
-        Integer userLoansCount = usersLoansCount.get(loanApplicationRequest.getRequester());
-
-        if (userLoansCount == null) {
-            usersLoansCount.put(loanApplicationRequest.getRequester(), 1);
-            LoanApplicationDecision loanApplicationDecision = new LoanApplicationDecision();
-            loanApplicationDecision.setAmount(loanApplicationRequest.getAmount());
-            loanApplicationDecision.setRequester(loanApplicationRequest.getRequester());
-            return new Record<>(loanApplicationDecision.getRequester(), loanApplicationDecision, context.currentStreamTimeMs());
-        } else if (userLoansCount == 1) {
-            usersLoansCount.put(loanApplicationRequest.getRequester(), 2);
-            LoanApplicationDecision loanApplicationDecision = new LoanApplicationDecision();
-            loanApplicationDecision.setAmount(loanApplicationRequest.getAmount().multiply(new BigDecimal("0.8")));
-            loanApplicationDecision.setRequester(loanApplicationRequest.getRequester());
-            return new Record<>(loanApplicationDecision.getRequester(), loanApplicationDecision, context.currentStreamTimeMs());
-        } else {
-            usersLoansCount.put(loanApplicationRequest.getRequester(), ++userLoansCount);
-            LoanApplicationDecision loanApplicationDecision = new LoanApplicationDecision();
-            loanApplicationDecision.setAmount(loanApplicationRequest.getAmount().multiply(new BigDecimal("0.5")));
-            loanApplicationDecision.setRequester(loanApplicationRequest.getRequester());
-            return new Record<>(loanApplicationDecision.getRequester(), loanApplicationDecision, context.currentStreamTimeMs());
-        }
-    }
-
 }
