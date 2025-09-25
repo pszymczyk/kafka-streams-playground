e876d637c02751aa1d9b280dba3ecff895a8a80c
Pawel Szymczyk <pawel@DESKTOP-3T909GL.>
2025-09-25 21:55:48 +0200

    remove implementation

--- src/main/java/com/pszymczyk/app7/App7.java
+++ src/main/java/com/pszymczyk/app7/App7.java
@@ -40,19 +40,7 @@ class App7 {
     static StreamsBuilder buildKafkaStreamsTopology() {
         StreamsBuilder builder = new StreamsBuilder();
 
-        KStream<Void, Message> messagesStream = builder.stream(APP_7_SOURCE, Consumed.with(Serdes.Void(), MessageSerde.newSerde()));
-
-        KTable<Windowed<String>, Inbox> threeDaysInbox = messagesStream
-            .groupBy((nullKey, message) -> message.receiver())
-            .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofDays(3)).advanceBy(Duration.ofDays(1)))
-            .aggregate(() -> new Inbox(new ArrayList<>()),
-                (key, message, inbox) -> inbox.add(message),
-                Materialized.with(Serdes.String(), JsonSerdes.newSerdes(Inbox.class))
-            );
-
-        threeDaysInbox
-            .toStream((key, value) -> String.format("%s-%s-%s", key.window().startTime(), key.window().endTime(), key.key()))
-            .to(APP_7_SINK);
+        //TODO
 
         return builder;
     }
